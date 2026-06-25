e184f784679fef3ed6539e451272aae5cb4d49f2
Nethara Vidmantha <xnethara@gmail.com>
2026-06-25 11:01:30 +0530

    feat: add routing and new pages, update dependencies, and implement header component
    - Integrated React Router for navigation with routes for Home, Register, Login, Admin, Test, and Settings pages.
    - Added new dependencies: @react-oauth/google, @supabase/supabase-js, axios, react-hot-toast, react-icons, and tailwindcss.
    - Created Header component for navigation and user interaction.
    - Added Loader component for loading states.
    - Included new images in the public directory.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AdminPage from "./pages/adminPage";
+import HomePage from "./pages/homePage";
+import TestPage from "./pages/test";
+import LoginPage from "./pages/loginPage";
+import { Toaster } from "react-hot-toast";
+import RegisterPage from "./pages/registerPage";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import ForgetPassword from "./pages/forget-password";
+import UserSettings from "./pages/settings";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <div className="w-full min-h-screen bg-primary text-secondary">
+          <Toaster position="top-right" />
+          <Routes path="/">
+            <Route path="/*" element={<HomePage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/forget-password" element={<ForgetPassword />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/admin/*" element={<AdminPage />} />
+            <Route path="/test" element={<TestPage />} />
+            <Route path="/settings" element={<UserSettings />} />
+          </Routes>
+        </div>
+      </GoogleOAuthProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
